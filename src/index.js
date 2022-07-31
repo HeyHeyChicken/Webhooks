0324a5bbb61cb28d23c659589b0e7dd2c4ad1b1b
Antoine Duval <33424294+HeyHeyChicken@users.noreply.github.com>
2022-07-31 20:38:28 +0200

    Update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const LIBRARIES = {
+  Axios: require("axios"),
   Skill: require("../../../Libraries/Skill")
 };
 
@@ -11,6 +12,23 @@ class Webhooks extends LIBRARIES.Skill {
       // L'utilisateur envoie un message
       socket.on("cs_message", function(_message) {
         console.log(_message);
+        loop:
+        for(let i = 0; i < _settings.length; i++){
+          for(let j = 0; j < _settings[i].utterances.length; j++){
+            if(_settings[i].utterances[j] == _message.Content){
+              for(let k = 0; k < _settings[i].urls.length; k++){
+                LIBRARIES.Axios.get(_settings[i].urls[k]).then(res => {
+                  const TEXT = _settings[i].answers[Math.floor(Math.random() * _settings[i].answers.length)];
+                  socket.emit("sc_message", new LIBRARIES.Message(TEXT, true));
+                })
+                .catch(error => {
+                  console.error(error);
+                });
+              }
+              break loop;
+            }
+          }
+        }
       });
     });
   }
