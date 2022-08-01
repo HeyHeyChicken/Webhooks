bcf8b1029dfdb5d78cba99c9c4cad18a6a630056
Antoine Duval <33424294+HeyHeyChicken@users.noreply.github.com>
2022-08-01 07:03:57 +0200

    Update

--- src/index.js
+++ src/index.js
@@ -9,29 +9,23 @@ class Webhooks extends LIBRARIES.Skill {
     super(_main, _settings, _folder);
     const SELF = this;
 
-    _main.ClientIO.on("connection", function(socket){
-      // L'utilisateur envoie un message
-      socket.on("cs_message", function(_message) {
-        loop:
-        for(let i = 0; i < _settings.length; i++){
-          for(let j = 0; j < _settings[i].utterances.length; j++){
-            if(_settings[i].utterances[j] == _message.Content){
-              for(let k = 0; k < _settings[i].urls.length; k++){
-                LIBRARIES.Axios.get(_settings[i].urls[k]).then(res => {
-                  const TEXT = _settings[i].answers[Math.floor(Math.random() * _settings[i].answers.length)];
-                  socket.emit("sc_message", new LIBRARIES.Message(TEXT, true));
-                  _main.TTS(socket, TEXT);
-                })
-                .catch(error => {
-                  console.error(error);
-                });
-              }
-              break loop;
-            }
-          }
+    for(let i = 0; i < _settings.length; i++){
+      const INTENT = "webkooks.intent_" + i;
+      _main.Manager.addDocuments(INTENT, _settings[i].utterances);
+      _main.Manager.addAnswers(INTENT, _settings[i].answers);
+      _main.Manager.addAction(INTENT, function(_intent, _socket){
+        for(let k = 0; k < _settings[i].urls.length; k++){
+          LIBRARIES.Axios.get(_settings[i].urls[k]).then(res => {
+            const TEXT = _settings[i].answers[Math.floor(Math.random() * _settings[i].answers.length)];
+            _socket.emit("sc_message", new LIBRARIES.Message(TEXT, true));
+            _main.TTS(socket, TEXT);
+          })
+          .catch(error => {
+            console.error(error);
+          });
         }
       });
-    });
+    }
   }
 }
 
