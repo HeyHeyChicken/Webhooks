f21732ab81c3cba3b805504a2e709d1bada88628
Antoine Duval <33424294+HeyHeyChicken@users.noreply.github.com>
2022-08-01 07:14:03 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -16,9 +16,9 @@ class Webhooks extends LIBRARIES.Skill {
       _main.Manager.addAction(INTENT, function(_intent, _socket){
         for(let k = 0; k < _settings[i].urls.length; k++){
           LIBRARIES.Axios.get(_settings[i].urls[k]).then(res => {
-            const TEXT = _settings[i].answers[Math.floor(Math.random() * _settings[i].answers.length)];
-            _socket.emit("sc_message", new LIBRARIES.Message(TEXT, true));
-            _main.TTS(socket, TEXT);
+            //const TEXT = _settings[i].answers[Math.floor(Math.random() * _settings[i].answers.length)];
+            //_socket.emit("sc_message", new LIBRARIES.Message(TEXT, true));
+            //_main.TTS(_socket, TEXT);
           })
           .catch(error => {
             console.error(error);
